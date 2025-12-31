b0cd055a1392dc2e6ab68f1605c22221a922523f
dltldn333 <dltldn333@naver.com>
2025-12-31 20:25:13 +0900

    chore: Update build config & bump version to v0.1.1

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,32 @@
-import { defineConfig } from 'vite';
-import dts from 'vite-plugin-dts';
-import path from 'path';
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import path from "path";
 
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'MirageEngine',
-      fileName: (format) => `mirage-engine.${format}.js`,
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "MirageEngine",
+      fileName: (format) =>
+        `mirage-engine.${format === "es" ? "js" : "umd.js"}`,
     },
     rollupOptions: {
-      external: ['three'],
+      external: ["three"],
       output: {
         globals: {
-          three: 'THREE',
+          three: "THREE",
         },
       },
     },
   },
-  plugins: [dts()],
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
