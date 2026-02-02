988363e5d786ccfbfa1af6c8755792538f4dd1a8
dltldn333 <dltldn333@naver.com>
2026-02-03 02:14:52 +0900

    feat: solving delete legacy

--- packages/mirage-engine/src/types/flags.ts
+++
@@ -1,7 +0,0 @@
-//Bitmask Flags
-export const DIRTY_NONE = 0;
-export const DIRTY_RECT = 1 << 0; // 1: x, y, width, height (00000001)
-export const DIRTY_STYLE = 1 << 1; // 2: style (00000010)
-export const DIRTY_ZINDEX = 1 << 2; // 4: zIndex (00000100)
-export const DIRTY_STRUCTURE = 1 << 3; // 8: childNode add / delete (00001000)
-export const DIRTY_CONTENT = 1 << 4;   // 16: text content changed (00010000)
