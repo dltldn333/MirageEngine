bda94d497d95f0ba64c9bf0f92136b05d62c8ed3
dltldn333 <dltldn333@naver.com>
2026-05-30 01:58:03 +0900

    feat: mesh store method

--- packages/core/src/animation/MeshRegistry.ts
+++ packages/core/src/animation/MeshRegistry.ts
@@ -6,5 +6,19 @@ export class MeshRegistry {
   constructor() {
     this.store = new WeakMap();
   }
+  public register(element: HTMLElement, meshGroup: THREE.Group): void {
+    this.store.set(element, meshGroup);
+  }
 
+  public get(element: HTMLElement): THREE.Group | undefined {
+    return this.store.get(element);
+  }
+
+  public remove(element: HTMLElement): void {
+    this.store.delete(element);
+  }
+
+  public has(element: HTMLElement): boolean {
+    return this.store.has(element);
+  }
 }
